89e515c97ecf1d499b258b224d3a39fa812a44bd
loganyeh <yehlogan@gmail.com>
2026-03-06 16:12:11 -0500

    qs: header borders created

--- src/Pages/BrowsePage.jsx
+++ src/Pages/BrowsePage.jsx
@@ -9,7 +9,32 @@ function BrowsePage(){
             <div className="border-2 border-purple-600 h-screen w-screen">
 
                 {/* header */}
-                <div className="border-2 border-red-600 h-20 w-full"></div>
+                <div className="border-2 border-red-600 h-20 w-full flex">
+                    {/* logo section */}
+                    <div className="border-2 border-red-600 h-full w-1/3 flex justify-center items-center">
+                        <div className="border border-black h-full w-1/3 flex justify-center items-center">
+                            <div className="border border-black h-14 w-14"></div>
+                        </div>
+                    </div>
+                    {/* nav section */}
+                    <div className="border-2 border-red-600 h-full w-1/3 flex justify-center items-center text-sm font-medium">
+                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Home</div>
+                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Profile</div>
+                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Anime List</div>
+                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Manga List</div>
+                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Browse</div>
+                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Forum</div>
+                    </div>
+                    {/* profile settings section */}
+                    <div className="border-2 border-blue-600 h-full w-1/3 flex justify-center items-center">
+                        <div className="border-2 border-black h-full w-1/2 flex justify-end items-center">
+                            <div className="border border-black h-10 w-10 flex justify-center items-center"><i className='bx bx-search border border-black text-2xl'></i></div>
+                            <div className="border border-black h-10 w-10"></div>
+                            <div className="border border-black h-10 w-6 flex justify-center items-center"><i className='bx bx-chevron-down text-2xl' ></i></div>
+                            
+                        </div>
+                    </div>
+                </div>
 
 
                 {/* body */}
@@ -18,7 +43,7 @@ function BrowsePage(){
 
 
                 {/* footer */}
-                <div className="border-2 border-red-600 h-70 w-full"></div>
+                <div className="border-2 border-red-600 h-72 w-full"></div>
 
 
 
