e129c97dd78c3851047213ee0a7217dce5bda783
loganyeh <yehlogan@gmail.com>
2026-03-06 17:36:43 -0500

    update: created borders for searchs and filters section

--- src/Pages/BrowsePage.jsx
+++ src/Pages/BrowsePage.jsx
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 function BrowsePage(){
+
+    const filterNames = ["Search", "Genres", "Year", "Season", "Format", "Airing Status"];
 
     return(
         <>
@@ -28,9 +28,9 @@ function BrowsePage(){
                     {/* profile settings section */}
                     <div className="border-2 border-blue-600 h-full w-1/3 flex justify-center items-center">
                         <div className="border-2 border-black h-full w-1/2 flex justify-end items-center">
-                            <div className="border border-black h-10 w-10 flex justify-center items-center"><i className='bx bx-search border border-black text-2xl'></i></div>
-                            <div className="border border-black h-10 w-10"></div>
-                            <div className="border border-black h-10 w-6 flex justify-center items-center"><i className='bx bx-chevron-down text-2xl' ></i></div>
+                            <div className="border border-black h-12 w-12 flex justify-center items-center"><i className='bx bx-search border border-black text-2xl'></i></div>
+                            <div className="border border-black h-12 w-12"></div>
+                            <div className="border border-black h-12 w-6 flex justify-center items-center"><i className='bx bx-chevron-down text-2xl' ></i></div>
                             
                         </div>
                     </div>
@@ -41,10 +41,18 @@ function BrowsePage(){
                 <div className="border-2 border-blue-600 h-500 w-full flex flex-col justify-start items-center">
 
                     {/* search and filters */}
-                    <div className="border border-black h-36 w-10/12">
-                        <div className="border-2 border-red-600 h-full w-1/6 flex flex-col justify-center items-center">
-                            <span className="border border-black">Search</span>
-                            <div className="border border-black h-6 w-1/2"></div>
+                    <div className="border border-black h-36 w-10/12 flex justify-between">
+                        {filterNames.map((name, index) => {
+                            return <div className="border-2 border-red-600 h-full w-56 flex flex-col justify-center items-start">
+                                <div className="border border-black h-10 w-48 flex justify-start items-center text-lg font-semibold">{name}</div>
+                                <div className="border border-black h-10 w-48 rounded-xl"></div>
+                            </div>
+                        })}
+                        {/* filter */}
+                        <div className="border-2 border-red-600 h-full w-36 flex justify-end items-center">
+                            <div className="border-2 border-red-600 h-10 w-10 flex justify-center items-center rounded-xl">
+                                <i class='bx bx-slider text-2xl' ></i>
+                            </div>
                         </div>
                     </div>
 
@@ -56,27 +64,7 @@ function BrowsePage(){
                             <div className="border border-black h-full w-1/6">
                                 <div className="border border-red-600 h-4/5 w-10/12"></div>
                             </div>
-                            <div className="border border-black h-full w-1/6">
-                                <div className="border border-red-600 h-4/5 w-10/12"></div>
-                            </div>
-                            <div className="border border-black h-full w-1/6">
-                                <div className="border border-red-600 h-4/5 w-10/12"></div>
-                            </div>
-                            <div className="border border-black h-full w-1/6">
-                                <div className="border border-red-600 h-4/5 w-10/12"></div>
-                            </div>
-                            <div className="border border-black h-full w-1/6">
-                                <div className="border border-red-600 h-4/5 w-10/12"></div>
-                            </div>
-                            <div className="border border-black h-full w-1/6">
-                                <div className="border border-red-600 h-4/5 w-10/12"></div>
-                            </div>
                         </div>
-                    </div>
-
-
-                    <div className="border-4 border-black h-200 w-10/12">
-                        <div>TESTING</div>
                     </div>
 
 
