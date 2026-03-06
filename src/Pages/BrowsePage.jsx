d8a4a56ff9a49f2f083d0352aad4866543937c1d
loganyeh <yehlogan@gmail.com>
2026-03-06 17:05:08 -0500

    qs

--- src/Pages/BrowsePage.jsx
+++ src/Pages/BrowsePage.jsx
@@ -38,7 +38,49 @@ function BrowsePage(){
 
 
                 {/* body */}
-                <div className="border-2 border-red-600 h-500 w-full"></div>
+                <div className="border-2 border-blue-600 h-500 w-full flex flex-col justify-start items-center">
+
+                    {/* search and filters */}
+                    <div className="border border-black h-36 w-10/12">
+                        <div className="border-2 border-red-600 h-full w-1/6 flex flex-col justify-center items-center">
+                            <span className="border border-black">Search</span>
+                            <div className="border border-black h-6 w-1/2"></div>
+                        </div>
+                    </div>
+
+
+                    {/* anime posters */}
+                    <div className="border border-black h-96 w-10/12">
+                        <div className="h-1/12 w-full text-xl font-medium">Trending Now</div>
+                        <div className="h-11/12 w-full flex">
+                            <div className="border border-black h-full w-1/6">
+                                <div className="border border-red-600 h-4/5 w-10/12"></div>
+                            </div>
+                            <div className="border border-black h-full w-1/6">
+                                <div className="border border-red-600 h-4/5 w-10/12"></div>
+                            </div>
+                            <div className="border border-black h-full w-1/6">
+                                <div className="border border-red-600 h-4/5 w-10/12"></div>
+                            </div>
+                            <div className="border border-black h-full w-1/6">
+                                <div className="border border-red-600 h-4/5 w-10/12"></div>
+                            </div>
+                            <div className="border border-black h-full w-1/6">
+                                <div className="border border-red-600 h-4/5 w-10/12"></div>
+                            </div>
+                            <div className="border border-black h-full w-1/6">
+                                <div className="border border-red-600 h-4/5 w-10/12"></div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="border-4 border-black h-200 w-10/12">
+                        <div>TESTING</div>
+                    </div>
+
+
+                </div>
 
 
 
