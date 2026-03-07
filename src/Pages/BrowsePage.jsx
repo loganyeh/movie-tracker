dbca6f900b9d13aa528896ab307fdd80b20a2b30
loganyeh <yehlogan@gmail.com>
2026-03-07 13:07:33 -0500

    wip: shell 1.0 with borders

--- src/Pages/BrowsePage.jsx
+++ src/Pages/BrowsePage.jsx
@@ -76,62 +76,57 @@ function BrowsePage(){
                     </div>
 
                     {/* TOP 100 TRENDING SECTION */}
-                    <div className="border-2 border-black h-100 w-10/12">
-                        {top10Movies.map(() => {
-                            return <div>
-                                {/* header title */}
+                    <div className="border-2 border-yellow-600 h-auto w-10/12 mb-10">
+                        {/* header title */}
                         <div className="border-2 border-black h-10 w-full flex">
                             <div className="border-2 border-red-600 h-full w-1/2 flex justify-start items-center text-lg font-semibold">TOP 100 MOVIES</div>
                             <div className="border-2 border-red-600 h-full w-1/2 flex justify-end items-center text-sm font-normal">View All</div>
                         </div>
-                        
-                        {/* ranking section */}
-                        <div className="border-2 border-red-600 h-20 w-full flex">
-                            {/* ranking */}
-                            <div className="border-2 border-red-600 h-full w-1/12 flex justify-center items-center">#1</div>
-                            {/* poster info */}
-                            <div className="border-2 border-red-600 h-full w-11/12 flex">
-                                {/* poster */}
-                                <div className="border border-black h-full w-1/12 flex justify-center items-center">
-                                    <div className="border border-red-600 h-11/12 w-1/2 rounded"></div>
-                                </div>
-                                {/* title and genre */}
-                                <div className="border border-black h-full w-6/12">
-                                    <div className="border border-black h-1/2 w-full flex justify-start items-center">Frieren's Beyond Journey</div>
-                                    <div className="border border-black h-1/2 w-full flex justify-start items-center">
-                                        <div className="border border-black h-6 w-auto px-4 mr-2 rounded-xl flex justify-center items-center text-xs font-semibold">adventure</div>
-                                        <div className="border border-black h-6 w-auto px-4 mr-2 rounded-xl flex justify-center items-center text-xs font-semibold">drama</div>
-                                        <div className="border border-black h-6 w-auto px-4 mr-2 rounded-xl flex justify-center items-center text-xs font-semibold">fantasy</div>
+
+                        {top10Movies.map(() => {
+                            return <div className="border-2 border-red-600 h-20 w-full flex mb-6">
+                                    {/* ranking */}
+                                    <div className="border-2 border-red-600 h-full w-1/12 flex justify-center items-center">#1</div>
+                                    {/* poster info */}
+                                    <div className="border-2 border-red-600 h-full w-11/12 flex">
+                                        {/* poster */}
+                                        <div className="border border-black h-full w-1/12 flex justify-center items-center">
+                                            <div className="border border-red-600 h-11/12 w-1/2 rounded"></div>
+                                        </div>
+                                        {/* title and genre */}
+                                        <div className="border border-black h-full w-6/12">
+                                            <div className="border border-black h-1/2 w-full flex justify-start items-center">Frieren's Beyond Journey</div>
+                                            <div className="border border-black h-1/2 w-full flex justify-start items-center">
+                                                <div className="border border-black h-6 w-auto px-4 mr-2 rounded-xl flex justify-center items-center text-xs font-semibold">adventure</div>
+                                                <div className="border border-black h-6 w-auto px-4 mr-2 rounded-xl flex justify-center items-center text-xs font-semibold">drama</div>
+                                                <div className="border border-black h-6 w-auto px-4 mr-2 rounded-xl flex justify-center items-center text-xs font-semibold">fantasy</div>
+                                            </div>
+                                        </div>
+                                        {/* movie info */}
+                                        <div className="border border-black h-full w-5/12 flex">
+                                            <div className="border border-black h-full w-1/12 flex justify-center items-center">
+                                                <i className='bx bx-circle text-3xl text-green-600 bg-green-600 rounded-full'></i>
+                                            </div>
+                                            <div className="border border-black h-full w-1/4 mr-8">
+                                                <div className="border border-black h-1/2 w-full flex justify-start items-center text-lg font-medium">91%</div>
+                                                <div className="border border-black h-1/2 w-full flex justify-start items-center text-xs font-semibold">391956 Users</div>
+                                            </div>
+                                            <div className="border border-black h-full w-1/4 mr-8">
+                                                <div className="border border-black h-1/2 w-full flex justify-start items-center text-lg font-medium">91%</div>
+                                                <div className="border border-black h-1/2 w-full flex justify-start items-center text-xs font-semibold">391956 Users</div>
+                                            </div>
+                                            <div className="border border-black h-full w-1/4 mr-8">
+                                                <div className="border border-black h-1/2 w-full flex justify-start items-center text-lg font-medium">91%</div>
+                                                <div className="border border-black h-1/2 w-full flex justify-start items-center text-xs font-semibold">391956 Users</div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                {/* movie info */}
-                                <div className="border border-black h-full w-5/12 flex">
-                                    <div className="border border-black h-full w-1/12 flex justify-center items-center">
-                                        <i className='bx bx-circle text-3xl text-green-600 bg-green-600 rounded-full'></i>
-                                    </div>
-                                    <div className="border border-black h-full w-1/4 mr-8">
-                                        <div className="border border-black h-1/2 w-full flex justify-start items-center text-lg font-medium">91%</div>
-                                        <div className="border border-black h-1/2 w-full flex justify-start items-center text-xs font-semibold">391956 Users</div>
-                                    </div>
-                                    <div className="border border-black h-full w-1/4 mr-8">
-                                        <div className="border border-black h-1/2 w-full flex justify-start items-center text-lg font-medium">91%</div>
-                                        <div className="border border-black h-1/2 w-full flex justify-start items-center text-xs font-semibold">391956 Users</div>
-                                    </div>
-                                    <div className="border border-black h-full w-1/4 mr-8">
-                                        <div className="border border-black h-1/2 w-full flex justify-start items-center text-lg font-medium">91%</div>
-                                        <div className="border border-black h-1/2 w-full flex justify-start items-center text-xs font-semibold">391956 Users</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                            </div>
                         })}
                     </div>
 
 
                 </div>
-
-
 
                 {/* footer */}
                 <Footer />
