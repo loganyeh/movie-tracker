b0231f9461f26fc2cad94d7e49c0a200a5470436
loganyeh <yehlogan@gmail.com>
2026-03-07 13:02:28 -0500

    qs

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,11 +1,12 @@
+import FooterColumn from "./FooterComponents/FooterColumn";
 
 function Footer(){
 
+    // column row names
     const columnOne = ["Logout", "Donate", "AniList.co", "AniChart.net"];
     const columnTwo = ["Apps", "Site Stats", "Recommendations", "API"];
     const columnThree = ["Discord", "Twitter", "Bluesky", "Facebook", "GitHub"];
     const columnFour = ["Add Data", "Moderators", "Contact", "Terms & Privacy", "Site Map"];
-
 
     return(
         <>
@@ -26,12 +27,10 @@ function Footer(){
 
                 {/* right side */}
                 <div className="border-2 border-black h-full w-1/2 flex justify-center items-center">
-                    <div className="border border-red-600 h-2/3 w-1/4">
-                        <div className="text-md font-medium mb-3">Logout</div>
-                        <div className="text-md font-medium mb-3">Donate</div>
-                        <div className="text-md font-medium mb-3">AniList.co</div>
-                        <div className="text-md font-medium mb-3">AniChart.net</div>
-                    </div>
+                    <FooterColumn column={columnOne} />
+                    <FooterColumn column={columnTwo} />
+                    <FooterColumn column={columnThree} />
+                    <FooterColumn column={columnFour} />
                 </div>
 
             </div>
--- src/Component/FooterComponents/FooterColumn.jsx
+++ src/Component/FooterComponents/FooterColumn.jsx
@@ -1,9 +1,13 @@
 
-function FooterColumn(){
+function FooterColumn({ column }){
 
     return(
         <>
-            
+            <div className="border border-red-600 h-2/3 w-1/4">
+                {column.map((rowName, index) => {
+                    return <div key={index} className="text-md font-semibold mb-3">{rowName}</div>
+                })}
+            </div>
         </>
     )
 }
