1bacdcfd87acc283e2f06f431c55945c9c466e79
loganyeh <yehlogan@gmail.com>
2026-03-07 13:52:33 -0500

    qs & update: header and search/filters done

--- src/Pages/BrowsePage.jsx
+++ src/Pages/BrowsePage.jsx
@@ -2,57 +2,76 @@ import Footer from "../Component/Footer";
 
 function BrowsePage(){
 
-    const filterNames = ["Search", "Genres", "Year", "Season", "Format", "Airing Status"];
+    const filterNames = ["Genres", "Year", "Season", "Format", "Airing Status"];
+    const filterData = [
+        {title: "Search", placeholder: ""},
+        {title: "Genres", placeholder: "Any"}
+    ]
     const top10Movies = ["", "", "", "", "", "", "", "", "", "",];
 
     return(
         <>
             {/*  */}
-            <div className="border-2 border-purple-600 h-screen w-screen">
+            <div className="min-h-screen w-screen bg-gray-200">
 
                 {/* header */}
-                <div className="border-2 border-red-600 h-20 w-full flex">
+                <div className="h-20 w-full flex bg-gray-800 text-gray-300">
                     {/* logo section */}
-                    <div className="border-2 border-red-600 h-full w-1/3 flex justify-center items-center">
-                        <div className="border border-black h-full w-1/3 flex justify-center items-center">
-                            <div className="border border-black h-14 w-14"></div>
+                    <div className="h-full w-1/3 flex justify-center items-center">
+                        <div className="h-full w-1/3 flex justify-center items-center">
+                            <div className="border-2 border-white h-14 w-14 bg-white"></div>
                         </div>
                     </div>
                     {/* nav section */}
-                    <div className="border-2 border-red-600 h-full w-1/3 flex justify-center items-center text-sm font-medium">
-                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Home</div>
-                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Profile</div>
-                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Anime List</div>
-                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Manga List</div>
-                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Browse</div>
-                        <div className="border border-black h-full w-1/6 flex justify-center items-center">Forum</div>
+                    <div className="h-full w-1/3 flex justify-center items-center text-sm font-medium">
+                        <div className="h-full w-1/6 flex justify-center items-center"><span className="cursor-pointer hover:text-white">Home</span></div>
+                        <div className="h-full w-1/6 flex justify-center items-center"><span className="cursor-pointer hover:text-white">Profile</span></div>
+                        <div className="h-full w-1/6 flex justify-center items-center"><span className="cursor-pointer hover:text-white">Anime List</span></div>
+                        <div className="h-full w-1/6 flex justify-center items-center"><span className="cursor-pointer hover:text-white">Manga List</span></div>
+                        <div className="h-full w-1/6 flex justify-center items-center"><span className="cursor-pointer hover:text-white">Browse</span></div>
+                        <div className="h-full w-1/6 flex justify-center items-center"><span className="cursor-pointer hover:text-white">Forum</span></div>
                     </div>
                     {/* profile settings section */}
-                    <div className="border-2 border-blue-600 h-full w-1/3 flex justify-center items-center">
-                        <div className="border-2 border-black h-full w-1/2 flex justify-end items-center">
-                            <div className="border border-black h-12 w-12 flex justify-center items-center"><i className='bx bx-search border border-black text-2xl'></i></div>
-                            <div className="border border-black h-12 w-12"></div>
-                            <div className="border border-black h-12 w-6 flex justify-center items-center"><i className='bx bx-chevron-down text-2xl' ></i></div>
+                    <div className="h-full w-1/3 flex justify-center items-center">
+                        <div className="h-full w-1/2 flex justify-end items-center">
+                            <div className="h-12 w-12 flex justify-center items-center"><i className='bx bx-search text-2xl text-gray-200 cursor-pointer hover:text-white'></i></div>
+                            <div className="border-2 border-white h-12 w-12 bg-white rounded"></div>
+                            <div className="h-12 w-6 flex justify-center items-center"><i className='bx bx-chevron-down text-2xl text-gray-200 cursor-pointer hover:text-white' ></i></div>
                             
                         </div>
                     </div>
                 </div>
 
                 {/* body */}
-                <div className="border-2 border-blue-600 h-500 w-full flex flex-col justify-start items-center">
+                <div className="h-auto w-full flex flex-col justify-start items-center">
 
                     {/* search and filters */}
-                    <div className="border border-black h-36 w-10/12 flex justify-between">
+                    <div className="h-36 w-10/12 flex justify-between">
+                        {/* search */}
+                        <div className="h-full w-56 flex flex-col justify-center items-start">
+                            <div className="h-10 w-48 flex justify-start items-center text-gray-700 text-md font-semibold">Search</div>
+                            {/* search input */}
+                            <div className="h-10 w-48 flex bg-gray-50 rounded-lg shadow-md">
+                                <div className="h-full w-2/12 flex justify-center items-center cursor-pointer"><i className='bx bx-search text-gray-500' ></i></div>
+                                <input type="text" className=" h-full w-10/12 pl-2 text-sm font-medium bg-gray-50 rounded-lg outline-none"/>
+                            </div>
+                        </div>
+                        {/* filter */}
                         {filterNames.map((name, index) => {
-                            return <div className="border-2 border-red-600 h-full w-56 flex flex-col justify-center items-start">
-                                <div className="border border-black h-10 w-48 flex justify-start items-center text-lg font-semibold">{name}</div>
-                                <div className="border border-black h-10 w-48 rounded-xl"></div>
+                            return <div key={index} className="h-full w-56 flex flex-col justify-center items-start">
+                                <div className="h-10 w-48 flex justify-start items-center text-gray-700 text-md font-semibold">{name}</div>
+                                {/* filters */}
+                                <div className="h-10 w-48 flex bg-gray-50 rounded-lg shadow-md">
+                                    <input type="text" placeholder="Any" className=" h-full w-10/12 pl-4 text-sm font-medium bg-gray-50 rounded-lg outline-none"/>
+                                    <div className="h-full w-2/12 flex justify-center items-center cursor-pointer"><i className='bx bx-chevron-down text-xl text-gray-400 hover:text-gray-500' ></i></div>
+                                </div>
                             </div>
                         })}
-                        {/* filter */}
-                        <div className="border-2 border-red-600 h-full w-36 flex justify-end items-center">
-                            <div className="border-2 border-red-600 h-10 w-10 flex justify-center items-center rounded-xl">
-                                <i class='bx bx-slider text-2xl' ></i>
+                        {/* filter icon*/}
+                        <div className="h-full w-36 flex flex-col justify-center items-end">
+                            <div className="h-10 w-full"></div>
+                            <div className="border border-gray-200 h-10 w-10 flex justify-center items-center bg-gray-50 rounded-xl shadow-md cursor-pointer">
+                                <i class='bx bx-slider text-2xl text-gray-400 hover:text-blue-400' ></i>
                             </div>
                         </div>
                     </div>
@@ -75,11 +94,11 @@ function BrowsePage(){
                         </div>
                     </div>
 
-                    {/* TOP 100 TRENDING SECTION */}
+                    {/* TOP 10 TRENDING SECTION */}
                     <div className="border-2 border-yellow-600 h-auto w-10/12 mb-10">
                         {/* header title */}
                         <div className="border-2 border-black h-10 w-full flex">
-                            <div className="border-2 border-red-600 h-full w-1/2 flex justify-start items-center text-lg font-semibold">TOP 100 MOVIES</div>
+                            <div className="border-2 border-red-600 h-full w-1/2 flex justify-start items-center text-lg font-semibold">TOP 10 MOVIES</div>
                             <div className="border-2 border-red-600 h-full w-1/2 flex justify-end items-center text-sm font-normal">View All</div>
                         </div>
 
