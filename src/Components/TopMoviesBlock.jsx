ddb6686939463020d0edfdce467cf6ea7208d620
loganyeh <yehlogan@gmail.com>
2026-03-09 15:03:46 -0400

    feat: BrowsePage static UI hover DONE

--- src/Components/TopMoviesBlock.jsx
+++ src/Components/TopMoviesBlock.jsx
@@ -1,7 +1,6 @@
 import MovieBlock from "./TopMoviesComponents/MovieBlock";
 
 function TopMoviesBlock({ data }){
-    const topMovieRankings = ["#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8", "#9", "#10"];
 
     return(
         <>
--- src/Components/TopMoviesComponents/MovieBlock.jsx
+++ src/Components/TopMoviesComponents/MovieBlock.jsx
@@ -3,6 +3,8 @@ import { movieRatingColor } from "../../Functions/function";
 
 function MovieBlock({ data }){
     const bubbleColors = ["bg-green-400", "bg-red-400", "bg-pink-400", "bg-red-500", "bg-red-400", "bg-yellow-200", "bg-blue-500", "bg-yellow-500", "bg-blue-300", "bg-yellow-200"];
+    const hoverBubbleColors = ["hover:text-green-400", "hover:text-red-400", "hover:text-pink-400", "hover:text-red-500", "hover:text-red-400", "hover:text-yellow-200", "hover:text-blue-500", "hover:text-yellow-500", "hover:text-blue-300", "hover:text-yellow-200"];
+
 
     return(
         <>
@@ -19,15 +21,17 @@ function MovieBlock({ data }){
                 <div className="h-full w-11/12 flex bg-white rounded shadow-md">
                     {/* poster */}
                     <div className="h-full w-1/12 flex justify-center items-center">
-                        <img className="h-11/12 w-1/2 rounded" src={`https://image.tmdb.org/t/p/w500${data.poster}`} alt={`${data.title}.jpg`} />
+                        <img className="h-11/12 w-1/2 rounded cursor-pointer" src={`https://image.tmdb.org/t/p/w500${data.poster}`} alt={`${data.title}.jpg`} />
                     </div>
 
                     {/* title and genre */}
                     <div className="h-full w-6/12">
-                        <div className="h-1/2 w-full flex justify-start items-end text-lg text-gray-600 font-medium">{data.title}</div>
+                        <div className="h-1/2 w-full flex justify-start items-end text-lg text-gray-600 font-medium">
+                            <span className={`${hoverBubbleColors[data.key]} cursor-pointer`}>{data.title}</span>
+                        </div>
                         <div className="h-1/2 w-full flex justify-start items-center">
                             {data.genre_ids.map((genre, index) => {
-                                return <div key={index} className={`h-6 w-auto px-4 mr-2 flex justify-center items-center ${bubbleColors[data.key]} text-xs text-gray-900 font-semibold rounded-xl shadow`}>{genreIDSwitch(genre)}</div>
+                                return <div key={index} className={`h-6 w-auto px-4 mr-2 flex justify-center items-center ${bubbleColors[data.key]} text-xs text-gray-900 font-semibold rounded-xl shadow cursor-pointer`}>{genreIDSwitch(genre)}</div>
                             })}
                         </div>
                     </div>
@@ -35,15 +39,14 @@ function MovieBlock({ data }){
                     {/* movie stats */}
                     <div className="h-full w-5/12 flex">
                         <div className="h-full w-1/12 flex justify-start items-center">
-                            <i className={`bx bx-circle text-2xl text-green-500 
+                            <i className={`bx bx-smile text-3xl text-green-500 
                                 ${movieRatingColor(data.vote_average)}
-                                rounded-full`}>
+                                rounded-full`} >    
                             </i>
                         </div>
                         <div className="h-full w-1/4 mr-8">
                             <div className="h-1/2 w-full flex justify-start items-end text-gray-600 text-md font-medium">
-                                <div className="mr-1 flex justify-center items-center">{data.vote_average}</div>
-                                <div className="h-full flex justify-center items-end"><i className='bx bxs-star text-yellow-300 text-2xl'></i></div>
+                                <div className="mr-1 flex justify-center items-center">{data.vote_average}%</div>
                             </div>
                             <div className="h-1/2 w-full flex justify-start items-start text-gray-400 text-xs font-normal">{data.vote_count} Votes</div>
                         </div>
