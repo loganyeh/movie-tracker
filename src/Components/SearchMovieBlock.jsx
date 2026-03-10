ff546ffcf0457ff88bf8d9bff9dbf50aabf1ad60
loganyeh <yehlogan@gmail.com>
2026-03-09 22:44:03 -0400

    update: Search static UI almost done

--- src/Components/SearchMovieBlock.jsx
+++ src/Components/SearchMovieBlock.jsx
@@ -7,18 +7,36 @@ function SearchMovieBlock({ data }){
         <>
             {/* <div className="border-2 border-yellow-400 h-200 w-10/12 mb-10">
             </div> */}
-            {/* left off here need to do something with h-96 to keep poster sizing the same */}
-                <div className="border-2 border-yellow-600 h-96 w-10/12 mb-6">
+                <div className="min-h-96 w-10/12 mb-6">
                     {/* trending now title/header */}
-                    <div className="border border-red-600 h-8 w-full flex">
-                        <div className="h-full w-1/2 flex justify-start items-center text-lg text-gray-700 font-medium">ADD QUERY SEARCH HERE</div>
+                    <div className="h-8 w-full flex mb-4">
+                        <div className="h-full w-1/2 flex justify-start items-center text-lg text-gray-700 font-medium">
+                            <div className="flex justify-center items-center mr-4"><i className='bx bxs-purchase-tag text-gray-400 text-2xl'></i></div>
+                            <div className="h-10/12 w-1/6 flex justify-center items-center text-sm text-gray-100 font-semibold bg-blue-400 rounded-l cursor-pointer">
+                                <span>Search: Query</span>
+                            </div>
+                            <div className="h-10/12 w-6 flex justify-center items-center bg-blue-400 rounded-r cursor-pointer"><i className='bx bx-x text-gray-100' ></i></div>
+                        </div>
                         <div className="h-full w-1/2 flex justify-end items-center text-xs text-gray-400 font-medium">
-                            <span className="cursor-pointer hover:text-gray-700">View All</span>
+                            <div className="flex justify-center items-center cursor-pointer hover:text-gray-500">
+                                <div className="h-full w-auto mr-2 flex flex-col justify-center items-center cursor-pointer">
+                                    <i className='bx bxs-up-arrow' ></i>
+                                    <i className='bx bxs-down-arrow' ></i>
+                                </div>
+                                <span className="mr-3 text-md">Popularity</span>
+                            </div>
+                            <span className="mr-2 text-2xl font-thin">|</span>
+
+                            <div className="h-full w-auto flex justify-center items-center">
+                                <i className='bx bxs-grid mr-1 text-3xl text-gray-400 hover:text-gray-500 cursor-pointer' ></i>
+                                <i className='bx bxs-grid-alt mr-1 text-3xl text-gray-400 hover:text-gray-500 cursor-pointer' ></i>
+                                <i className='bx bx-grid-horizontal mr-1 text-3xl text-gray-400 hover:text-gray-500 cursor-pointer' ></i>
+                            </div>
                         </div>
                     </div>
 
                     {/* movie posters */}
-                    <div className="border border-blue-600 h-11/12 w-full flex flex-wrap">
+                    <div className="h-11/12 w-full flex flex-wrap">
                         {data.map((data, index) => {
                             return <Poster key={index} data={data} />
                         })}
