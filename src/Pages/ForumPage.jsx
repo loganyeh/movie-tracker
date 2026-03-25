bcd8fe357b6a713436edfe64bb36717c3b96ac4a
loganyeh <yehlogan@gmail.com>
2026-03-24 21:20:15 -0400

    update: ForumPage static UI DONE

--- src/Pages/ForumPage.jsx
+++ src/Pages/ForumPage.jsx
@@ -10,6 +10,8 @@ function ForumPage(){
         "Site Feedback", "Bug Reports", "AniList Apps", "Misc",
     ];
 
+    const threadsData = ["Recently Active Threads", "Release Discussion Threads", "Newly Created Threads"];
+
     return(
         <>
             <div className="border-2 border-red-600 h-screen w-screen">
@@ -21,7 +23,7 @@ function ForumPage(){
                 <div className="border-4 border-blue-600 h-auto w-full flex justify-center items-center bg-gray-200">
                     
                     {/* container */}
-                    <div className="border-2 border-red-600 h-auto w-10/12 pb-96 flex justify-around items-start">
+                    <div className="border-2 border-red-600 h-auto w-10/12 pb-24 flex justify-around items-start">
 
                         {/* left */}
                         <div className="border-2 border-green-400 h-full w-2/12 flex flex-col justify-center items-center">
@@ -74,15 +76,28 @@ function ForumPage(){
 
                             
                             {/* forum threads */}
-
                             {/* Recently Active Threads */}
-                            {/* Release Discussion Threads */}
-                            {/* Newly Created Threads */}
+                            {threadsData.map((thread, index) => {
+                                return <div key={index} className="h-auto w-full mt-6">
+                                            {/* header */}
+                                            <div className="h-10 w-full flex justify-between items-center">
+                                                <div className="h-10 w-auto flex justify-center items-center text-gray-600 font-medium">{thread || "Recently Active Threads"}</div>
+                                                <div className="h-10 w-auto flex justify-center items-center text-gray-500 text-xs font-medium">View All</div>
+                                            </div>
 
-
-
-
-
+                                            {/* threads */}
+                                            {Array.from({length: 4}).map((_, index) => {
+                                                return <div key={index} className="h-auto w-full mb-4 bg-white shadow-md rounded">
+                                                            <div className="h-10 w-full pl-4 flex justify-start items-end text-gray-700 font-normal text-lg">Crunchyroll Release Schedule for Spring 2026 Season</div>
+                                                            <div className="h-10 w-full pl-6 pt-1 flex justify-start items-start text-gray-500 text-sm truncate">Crunchyroll announced on Tuesday that it is streaming the following anime (dub languages streaming at later date in parentheses) for the spring 2026 season: March 28 Agents</div>
+                                                            <div className="h-10 w-full pl-4 flex justify-start items-center">
+                                                                <div className="h-6 w-6 mr-2 bg-blue-400 rounded"></div>
+                                                                <div className="text-sm text-gray-700"><span className="text-blue-400">Hanni</span> replied 1 hour ago</div>
+                                                            </div>
+                                                        </div>
+                                            })}
+                                        </div>
+                            })}
                         </div>
 
                     </div>
