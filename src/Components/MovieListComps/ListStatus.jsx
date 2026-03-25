181c800adb3b339d9f2c494a1e2890312425a574
loganyeh <yehlogan@gmail.com>
2026-03-25 15:00:40 -0400

    wip/feat: breakpoints @ MovieListPage -> FilterSearchBar, ListStatus, FilterDropdown, SortDropdown

--- src/Components/MovieListComps/ListStatus.jsx
+++ src/Components/MovieListComps/ListStatus.jsx
@@ -5,13 +5,13 @@ function ListStatus({ title, category }){
     return(
         <>
             <div className="h-auto w-full mt-6">
-                <div className="h-10 w-full pl-2 flex justify-start items-center text-gray-500 font-light">{title}</div>
+                <div className="2xl:h-10 xl:h-8 w-full pl-2 flex justify-start items-center text-base text-gray-500 font-light">{title}</div>
 
                 {/* categorires for list */}
                 <div className="h-auto w-full flex flex-col justify-center items-center">
-                    <div className={`h-8 w-11/12 pl-2 my-2 flex justify-start items-center bg-white text-sm text-gray-400 shadow rounded`}>All</div>
+                    <div className={`2xl:h-8 xl:h-6 w-11/12 pl-2 my-2 flex justify-start items-center bg-white text-sm text-gray-400 shadow rounded`}>All</div>
                         {category?.map((cat, index) => {
-                            return <div key={index} className={`h-8 w-11/12 pl-2 my-1 flex justify-start items-center text-sm text-gray-400`}>{cat}</div>
+                            return <div key={index} className={`2xl:h-8 xl:h-6 w-11/12 pl-2 2xl:my-1 xl:my-2 flex justify-start items-center text-sm text-gray-400`}>{cat}</div>
                         })}
                 </div>
 
--- src/Components/MovieListComps/SortDropdown.jsx
+++ src/Components/MovieListComps/SortDropdown.jsx
@@ -5,12 +5,12 @@ function SortDropdown(){
     return(
         <>
             <div className="h-auto w-full mt-6">
-                <div className="h-10 w-full pl-2 flex justify-start items-center text-gray-500 font-light">Sort</div>
+                <div className="2xl:h-10 xl:h-8 w-full pl-2 flex justify-start items-center text-gray-500 font-light">Sort</div>
 
                 <div className="h-auto w-full flex flex-col justify-center items-center">
-                    <div className="h-8 w-11/12 my-2 px-2 flex justify-between items-center bg-white font-light text-md text-gray-400 shadow rounded">
+                    <div className="2xl:h-8 xl:h-6 w-full my-2 px-2 flex justify-between items-center bg-white font-light text-md text-gray-400 shadow rounded">
                         <div className="flex justify-center items-center text-sm">Score</div>
-                        <div className="flex justify-center items-center"><i className='bx bx-chevron-down text-xl' ></i></div>
+                        <div className="flex justify-center items-center"><i className='bx bx-chevron-down 2xl:text-xl xl:text-lg' ></i></div>
                     </div>
                 </div>
 
