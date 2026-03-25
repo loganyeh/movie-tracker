7b18f7738cbf299ac136f72aab1071e65c65295c
loganyeh <yehlogan@gmail.com>
2026-03-25 18:16:31 -0400

    qs.

--- src/Components/MovieInfoPageComps/InfoCards/StaffCard.jsx
+++ src/Components/MovieInfoPageComps/InfoCards/StaffCard.jsx
@@ -4,13 +4,13 @@ function StaffCard({ data }){
 
     return(
         <>
-            <div className="min-h-28 w-80 mr-10 mb-6 flex bg-white rounded shadow-md">
-                <div className="h-auto w-1/4 bg-blue-300 rounded-l">
+            <div className="border min-h-28 2xl:w-80 xl:w-5/12 mr-10 mb-6 flex bg-white rounded shadow-md">
+                <div className="border h-auto w-1/4 bg-blue-300 rounded-l">
                     <img className="h-auto w-auto text-xs text-gray-400 rounded-l" src={`https://image.tmdb.org/t/p/original${data?.profile_path}`} alt="No Image" />
                 </div>
-                <div className="min-h-28 w-3/4 flex justify-center items-center">
-                    <div className="h-1/2 w-full pl-1 flex justify-start items-center text-gray-600 text-sm">{ data?.name || "Author"}</div>
-                    <div className="h-1/2 w-full pl-1 flex justify-start items-center text-gray-500 text-xs">{ data?.job || "Original Creator"}</div>
+                <div className="border min-h-28 w-3/4 flex justify-center items-center">
+                    <div className="border h-1/2 w-full pl-1 flex justify-start items-center text-gray-600 text-sm">{ data?.name || "Author"}</div>
+                    <div className="border h-1/2 w-full pl-1 flex justify-start items-center text-gray-500 text-xs">{ data?.job || "Original Creator"}</div>
                 </div>
             </div>
         </>
