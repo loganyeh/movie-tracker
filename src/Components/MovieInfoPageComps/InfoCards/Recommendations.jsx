de313122d38a54348675da012ca9eb9fc1deeb42
loganyeh <yehlogan@gmail.com>
2026-03-25 20:19:55 -0400

    cleanup

--- src/Components/MovieInfoPageComps/InfoCards/Recommendations.jsx
+++ src/Components/MovieInfoPageComps/InfoCards/Recommendations.jsx
@@ -5,6 +5,7 @@ function Recommendations({ data }){
     return(
         <>
             <div className="h-auto w-11/12 mb-6">
+
                 <div className="h-10 w-full flex justify-start items-center text-gray-600 text-lg font-semibold">
                     <div className="h-full w-1/2 flex justify-start items-center">Recommendations</div>
                     <div className="h-full w-1/2 flex justify-end items-center">
@@ -14,11 +15,12 @@ function Recommendations({ data }){
                         </div>
                     </div>
                 </div>
+
                 {/* movie poster # 1 */}
                 <div className="min-h-auto w-full flex flex-wrap justify-start items-start">
                     {data?.results?.slice(7, 12).map((data, index) => {
                         return <div key={index} className="min-h-48 w-36 mr-8">
-                                    <div className="h-48 w-full mr-12 bg-blue-300 rounded shadow-md cursor-pointer"
+                                    <div className="h-48 w-full mr-12 bg-blue-300 rounded shadow-md"
                                         style={{
                                             backgroundImage: `url(https://image.tmdb.org/t/p/original${data?.poster_path})`,
                                             backgroundSize: "cover",
