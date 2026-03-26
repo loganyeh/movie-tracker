1211961caab1cd52fe9432d096d2f5f1476f1451
loganyeh <yehlogan@gmail.com>
2026-03-25 20:23:25 -0400

    test: vercel poster size for xl

--- src/Components/TrendingBlockComponents/Poster.jsx
+++ src/Components/TrendingBlockComponents/Poster.jsx
@@ -17,7 +17,7 @@ function Poster({ data }){
         <>
             <div className="h-auto w-1/6 flex flex-col justify-start items-center">
                 <Link to={`/movie`} onClick={handleClick} className="h-auto w-9/12 bg-white rounded-md">
-                    <img className="2xl:h-72 xl:h-56 w-full rounded-md cursor-pointer" src={`https://image.tmdb.org/t/p/w500${data.poster}`} alt={`${data.title}.jpg`} />
+                    <img className="2xl:h-72 xl:h-48 w-full rounded-md cursor-pointer" src={`https://image.tmdb.org/t/p/w500${data.poster}`} alt={`${data.title}.jpg`} />
                 </Link>
                 <div className="min-h-8 w-full 2xl:my-2 xl:my-1 flex justify-center items-center font-medium">
                     <div className="h-auto w-10/12 text-gray-500 text-md flex justify-center items-center">
