5b232cf5b83aae54eb9b7284cbc2fec021e356a0
loganyeh <yehlogan@gmail.com>
2026-04-09 20:54:54 -0400

    fix: breakpoints for Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,40 +8,36 @@ function Header(){
         {title: "TV List", url: "/movielist"},
         {title: "Browse", url: "/"},
         {title: "Forum", url: "/forum"},
-    ]
+    ];
 
     return(
         <>
-            <div className={`border-2 border-red-600 hidden 2xl:h-20 xl:h-16 w-full xl:flex bg-gray-900 text-gray-300`}>
+            <div className={`py-3 hidden xl:flex justify-center bg-gray-900 text-gray-300`}>
 
-                {/* logo section */}
-                <div className="h-full w-1/3 flex justify-center items-center">
-                    <div className="h-full w-1/3 flex justify-center items-center">
-                        <Link to={"/profile"}><img className="2xl:h-16 2xl:w-16 xl:h-12 xl:w-12 cursor-pointer" src="../Letterboxd.jpeg" alt="" /></Link>
+                {/* controls width compared to parent's width */}
+                <div className="w-full max-w-6xl flex justify-between">
+
+                    {/* logo section */}
+                    <Link to={"/profile"} className=""><img className="h-12 w-12 cursor-pointer" src="../Letterboxd.jpeg" alt="" /></Link>
+
+                    {/* nav section */}
+                    <div className="flex gap-12 items-center">
+                        {navSectionData.map((data, index) => {
+                            return <Link key={index} to={data.url} className="">
+                                <div className="font-medium text-sm tracking-wide cursor-pointer hover:text-white">{data.title}</div>
+                            </Link>
+                        })}
                     </div>
-                </div>
 
-                {/* nav section */}
-                <div className="h-full w-1/3 flex justify-center items-center text-sm font-medium">
-                    {navSectionData.map((data, index) => {
-                        return <Link key={index} to={data.url} className="h-full w-1/6 flex justify-center items-center">
-                            <span className="2xl:text-sm xl:text-xs cursor-pointer hover:text-white">{data.title}</span>
-                        </Link>
-                    })}
-                </div>
-
-                {/* profile settings section */}
-                <div className="h-full w-1/3 flex justify-center items-center">
-                    <div className="h-full w-1/2 flex justify-end items-center">
-                        <Link to={"/"} className="2xl:h-12 2xl:w-12 2xl:mr-0 xl:mr-1 flex justify-center items-center"><i className='bx bx-search 2xl:text-2xl xl:text-xl cursor-pointer hover:text-white'></i></Link>
-                        <div className="2xl:h-12 2xl:w-18 xl:h-10 xl:w-16 flex">
-                            <img className="h-full w-2/3 rounded" src="../hanni.jpg" alt="" />
-                            <div className="h-full w-1/3 flex justify-center items-center">
-                                <i className='bx bx-chevron-down 2xl:text-2xl xl:text-xl' ></i>
-                            </div>
-                        </div>
+                    {/* profile settings section */}
+                    <div className="flex gap-3 items-center">
+                        <Link to={"/"} className="flex"><i className='bx bx-search text-2xl cursor-pointer hover:text-white'></i></Link>
+                        <img className="h-10 w-10 rounded" src="../hanni.jpg" alt="" />
+                        <i className='bx bx-chevron-down text-2xl' ></i>
                     </div>
+
                 </div>
+
             </div>
         </>
     )
