12f1be8055207c8cdf8dd5a499a661c89422713e
loganyeh <yehlogan@gmail.com>
2026-04-11 01:58:38 -0400

    wip: MovieList breakpoints. update: breakpoint mobile @ ProfileBanner

--- src/Components/ProfilePageComps/ProfileBanner.jsx
+++ src/Components/ProfilePageComps/ProfileBanner.jsx
@@ -4,13 +4,9 @@ function ProfileBanner(){
 
     return(
         <>
-            <div className="2xl:h-64 xl:h-52 w-full flex justify-center items-end shadow" style={{ backgroundImage: `url(/newJeansBanner.jpg)`, backgroundSize: "cover", backgroundPosition: "center",}}>
-                <div className="min-h-1/2 w-10/12 flex justify-start items-end">
-                    <div className="2xl:h-36 2xl:w-36 xl:h-28 flex justify-center items-end">
-                        <img className="2xl:h-36 xl:h-28 rounded-t" src={`/hanni.jpg`} alt="" />
-                    </div>
-                    <div className="2xl:h-12 2xl:w-32 xl:h-10 xl:w-24 flex justify-center items-center text-gray-700 2xl:text-2xl xl:text-xl font-semibold">Hanni</div>
-                </div>
+            <div className="h-[272px] px-6 flex gap-6 justify-start items-end shadow bg-cover bg-center" style={{ backgroundImage: `url(/newJeansBanner.jpg)`}}>
+                <div className="w-36 aspect-square rounded-t bg-center bg-cover" style={{ backgroundImage: `url(${`/hanni.jpg`})`}}></div>
+                <div className="py-5 text-gray-700 text-xl font-semibold tracking-wide">hanni</div>
             </div>
         </>
     )
