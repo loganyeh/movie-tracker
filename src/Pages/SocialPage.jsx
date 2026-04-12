9d2ad61abb17ba567d5c32e2f49d9fb90ea29f9b
loganyeh <yehlogan@gmail.com>
2026-04-12 00:56:54 -0400

    update: breakpoints @ SocialPage

--- src/Pages/SocialPage.jsx
+++ src/Pages/SocialPage.jsx
@@ -22,14 +22,14 @@ function SocialPage(){
             <div className="pb-96 flex justify-center bg-gray-200">
 
                 {/* body */}
-                <div className="p-5 w-full max-w-7xl 2xl:max-w-[1400px] flex gap-10 bg-gray-200">
+                <div className="p-5 w-full max-w-7xl 2xl:max-w-[1400px] flex flex-col md:flex-row gap-10 bg-gray-200">
                         {/* left */}
-                        <div className="w-full max-w-[210px]">
+                        <div className="w-full md:max-w-[210px]">
                             <ListStatus title={"Social"} category={socialListData} />
                         </div>
 
                         {/* right */}
-                        <div className="h-96 flex flex-1 justify-center items-center text-2xl text-gray-500 font-normal">
+                        <div className="flex flex-1 justify-center items-center text-lg text-gray-500 font-medium">
                             Following 0 users ｡ﾟヽ(ﾟ´Д｀)ﾉﾟ｡
                         </div>
 
