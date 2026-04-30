9bf9c4cc887caa02f5949f21e8dfc4a12e838bb6
loganyeh <yehlogan@gmail.com>
2026-04-29 20:10:36 -0400

    wip: POST request @ Review. end: 260429

--- src/Components/MovieInfoPageComps/InfoCards/Review.tsx
+++ src/Components/MovieInfoPageComps/InfoCards/Review.tsx
@@ -18,12 +18,21 @@ function Review(){
         getReviews();
     }, [])
 
+    function onClickReview(){
+        console.log("Clicked Create a review");
+        
+    }
+
     return(
         <>
             <div className="border border-red-600 flex gap-2 flex-col">
 
                 {/* reviews title */}
-                <div className="font-semibold text-gray-600">Reviews</div>
+                <div className="flex justify-between items-center text-gray-600">
+                    <div className="font-semibold">Reviews</div>
+                    <div onClick={() => onClickReview()} className="text-xs font-light cursor-pointer hover:text-gray-400">Create new review</div>
+                    {/* left off here to create POST request to add a review */}
+                </div>
 
                 {/* REVIEW CARD */}
                 <div className="flex gap-4 flex-col">
