0ffd0a9918832af39c6216aa6a930463669fe789
loganyeh <yehlogan@gmail.com>
2026-05-09 14:34:07 -0400

    edit: start PUT request

--- src/Components/MovieInfoPageComps/InfoCards/ThreadCard.tsx
+++ src/Components/MovieInfoPageComps/InfoCards/ThreadCard.tsx
@@ -10,7 +10,6 @@ type ThreadCardProp = {
 }
 
 function ThreadCard({ thread, deleteThread, refreshThreads }: ThreadCardProp ){
-    
     const [isComment, setIsComment] = useState(false);
     const [isEditComment, setIsEditComment] = useState(false);
     const [changeComment, setChangeComment] = useState("");
@@ -34,21 +33,6 @@ function ThreadCard({ thread, deleteThread, refreshThreads }: ThreadCardProp ){
     async function deleteComment(id: number, commentID: number){
         await fetch(`http://localhost:3000/threads/${id}/comments/${commentID}`, {
             method: "DELETE"
-        });
-
-        refreshThreads();
-    };
-
-    async function editComment(id: number, commentID: number){
-        setIsEditComment(prev => !prev);
-        await fetch(`http://localhost:3000/threads/${id}/comments/${commentID}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                comment: changeComment
-            })
         });
 
         refreshThreads();
@@ -114,32 +98,15 @@ function ThreadCard({ thread, deleteThread, refreshThreads }: ThreadCardProp ){
                     
                     {thread.comments.map((comment, index) => {
                         return <div key={index} className="flex justify-end items-start gap-2 p-3 text-sm text-gray-600">
-                                    {isEditComment 
-                                            ? 
-                                            <input type="text" onChange={(e) => setChangeComment(e.target.value)} className="border border-red-600 p-5 w-full" value={changeComment}/>
-                                            : 
-                                            <div className="border border-green-600">{comment.comment}</div> 
-                                    }
+                                    <div className="p-5 w-full">{comment.comment}</div> 
                                     <div className="flex flex-col items-center gap-2">
                                         <div className="shrink-0 w-6 h-6 bg-blue-300 rounded"></div>
-                                        
-                                        <i onClick={() => {editComment(thread.id, comment.id), setChangeComment(comment.comment)}} className='bx bx-edit-alt cursor-pointer hover:text-red-500' ></i>
+                                        <i className='bx bx-edit-alt cursor-pointer hover:text-red-500' ></i>
                                         <i onClick={() => deleteComment(thread.id, comment.id)} className='bx bx-trash cursor-pointer hover:text-red-500' ></i>
                                     </div>
                                 </div>
                     })}
-                    
-                    {/* {Array.from({length: 1}).map((_, index) => {
-                        return <div key={index} className="flex justify-end items-start gap-2 p-3 text-sm text-gray-600">
-                                    <div className=""></div>
-                                    <div className="flex flex-col items-center gap-2">
-                                        <div className="shrink-0 w-6 h-6 bg-blue-300 rounded"></div>
-                                        <i className='bx bx-edit-alt cursor-pointer hover:text-red-500' ></i>
-                                        <i className='bx bx-trash cursor-pointer hover:text-red-500' ></i>
-                                    </div>
-                                </div>
-                    })} */}
-                    
+
                 </div>
 
             </div>
